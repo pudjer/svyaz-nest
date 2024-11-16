91f3847adc6a06e553a7d98a8125ca16aa8388b2
Vladislav <tirettur@gmail.com>
2024-11-16 18:47:44 +0300

    dfdfdf

--- src/calls/calls.gateway.ts
+++ src/calls/calls.gateway.ts
@@ -11,20 +11,20 @@ import { Server, Socket } from 'socket.io';
 import { UserService } from 'src/users/users.service';
 
 type Common = {
-  Id: string,
-  PeerId: string,
-  SocketId: string,
+  id: string,
+  offer: unknown,
+  socket: Socket,
 }
 type Client = {
   timeEntered: Date
 } & Common
 
-type Queue = Map<string, Client>
-type Pool = Map<string, Common>
+type Queue = Map<Socket, Client>
+type Pool = Map<Socket, Common>
 
 type ClientData = {
   token: string
-  id: string
+  offer: string
 }
 
 @WebSocketGateway(90, { cors: true })
@@ -34,24 +34,85 @@ export class CallsGateway implements OnGatewayDisconnect {
 
   queue: Queue = new Map()
   pool: Pool = new Map()
+
   @WebSocketServer()
   server: Server;
 
   decodeJWT(jwt: string){
     return this.userService.validateJWT(jwt)
   }
+  makeCall(operator: Common, client: Client){
+    this.pool.delete(operator.socket)
+    this.queue.delete(client.socket)
+    this.handleSignaling(operator.socket, client.socket)
+    this.handleSignaling(client.socket, operator.socket)
+    operator.socket.emit("offer", client.offer)
+  }
+  enterOperator(operator: Common){
+
+    if(this.queue.size){
+      const last = this.queue.entries().next().value[1]
+      this.makeCall(operator, last)
+    }else{
+      this.pool.set(operator.socket, operator)
+    }
+  }
+  enterClient(client: Client){
+    if(this.pool.size){
+      const last = this.pool.entries().next().value[1]
+      this.makeCall(last, client)
+    }else{
+      this.queue.set(client.socket, client)
+    }
+  }
 
   @SubscribeMessage('entered')
-  async handleClientCall(@MessageBody() data: string, @ConnectedSocket() client: Socket) {
-    const {token, id} = JSON.parse(data)
+  async handleClientCall(@MessageBody() data: ClientData, @ConnectedSocket() socket: Socket) {
+    const {token, offer} = data
     const user = this.decodeJWT(token)
+    if(user.isAdmin){
+      const operator: Common = {
+        id: user._id,
+        offer: offer,
+        socket: socket,
+      }
+      this.enterOperator(operator)
+    }else{
+      const client: Client = {
+        id: user._id,
+        offer: offer,
+        socket: socket,
+        timeEntered: new Date()
+      }
+      this.enterClient(client)
+    }
+
 
   }
 
-  handleDisconnect(client: any) {
-    console.log("dis")
+  handleDisconnect(client: Socket) {
+    this.queue.delete(client)
+    this.pool.delete(client)
   }
 
-
+  handleSignaling(socket1: Socket, socket2: Socket){
+    console.log("soedinenie")
+    socket1.on('offer', (offer) => {
+      socket2.emit('offer', offer);
+    });
+  
+    socket1.on('answer', (answer) => {
+      socket2.emit('answer', answer);
+    });
+  
+    socket1.on('ice-candidate', (candidate) => {
+      socket2.emit('ice-candidate', candidate);
+    });
+  
+    socket1.on('disconnect', () => {
+      socket2.disconnect()
+    });
+  
+  }
 
 }
